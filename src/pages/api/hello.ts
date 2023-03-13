b8ba0e18ebef96430fd06fc4fa9bee7c0b237a65
Branko <saicoder.net@gmail.com>
2023-03-13 19:19:15 +0100

    T2

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Connection, PublicKey, Transaction } from '@solana/web3.js'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -6,9 +7,29 @@ type Data = {
   icon: string
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method === 'GET') {
+    res.status(200).json({
+      label: 'John Doe',
+      icon: 'https://chaintree.vercel.app/_next/image?url=https%3A%2F%2Fwww.arweave.net%2FcbKLtWO1-MdkBhnqF5PvEm2Su3-YcfwWewUt5Z4mP_4%3Fext%3Dpng&w=3840&q=75',
+    })
+
+    return
+  }
+
+  const connection = new Connection(process.env.NEXT_PUBLIC_SOLANA_ENDPOINT!)
+  const { blockhash } = await connection.getLatestBlockhash('finalized')
+
+  const tx = new Transaction({
+    feePayer: new PublicKey('CXxXQmjeyjPDVRdL2mENi2cDzHwSeFLbWazA2T9bCAV'),
+    recentBlockhash: blockhash,
+  })
+
+  const serializedTransaction = tx.serialize({ requireAllSignatures: false })
+  const base64 = serializedTransaction.toString('base64')
+
   res.status(200).json({
-    label: 'John Doe',
-    icon: 'https://chaintree.vercel.app/_next/image?url=https%3A%2F%2Fwww.arweave.net%2FcbKLtWO1-MdkBhnqF5PvEm2Su3-YcfwWewUt5Z4mP_4%3Fext%3Dpng&w=3840&q=75',
+    transaction: base64,
+    message: 'Thanks for your order!',
   })
 }
