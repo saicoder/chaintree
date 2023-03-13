afe92d5ffbf2d53e586a501559159c81f9c05eae
Branko <saicoder.net@gmail.com>
2023-03-13 18:44:57 +0100

    Example API

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,12 +2,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  label: string
+  icon: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({
+    label: 'John Doe',
+    icon: 'https://chaintree.vercel.app/_next/image?url=https%3A%2F%2Fwww.arweave.net%2FcbKLtWO1-MdkBhnqF5PvEm2Su3-YcfwWewUt5Z4mP_4%3Fext%3Dpng&w=3840&q=75',
+  })
 }
